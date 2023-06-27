f0cbeaf372c76625e062d7f45e1dd164d9fd9c03
quyet <ngovanquyet185@gmail.com>
2023-06-27 16:35:42 +0700

    lan 2

--- apiRouter.js
+++ apiRouter.js
@@ -2,15 +2,15 @@ const express = require('express')
 var router = express.Router()
 
 router.get('/',(req,res)=>{
-    res.json('oke dcm')
+    res.json('oke ')
 })
 
 router.get('/product',(req,res)=>{
-    res.json('oke dcm1')
+    res.json('oke 1')
 })
 
 router.get('/10',(req,res)=>{
-    res.json('oke dcm10')
+    res.json('oke 10')
 })
 
 module.exports = router
